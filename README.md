a93ec2784102e20e59a9e8de2a7c444a6917352a
ish2001 <48029530+ish2001@users.noreply.github.com>
2021-04-03 22:08:32 +0530

    Changes

--- README.md
+++ README.md
@@ -15,6 +15,11 @@
     <img src="https://badgen.net/badge/Gatsby/2.23.12/purple" alt="Gatsby version" />
   </a>
   
+   <a href="https://github.com/ish2001/test/pulls">
+     <img src="https://badgen.net/github/prs/ish2001/test" alt="Pull Request Amount">
+   </a>
+   <img src="https://badgen.net/github/last-commit/ish2001/test" alt="Last commit date" />
+    
   <a href="./LICENSE">
     <img src="https://badgen.net/badge/license/MIT/blue" alt="License">
   </a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text, Em } from "@quarkly/widgets";
+import { Theme, Link, Text, Span, Strong } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
 import { Section } from "@quarkly/components";
@@ -36,20 +36,36 @@ export default (() => {
 			</Text>
 			<Text
 				as="h1"
-				margin="78px 350px 32px 0px"
+				margin="78px 150px 32px 0px"
 				font="normal 900 72px/1.2 --fontFamily-sans"
 				md-font="--headline2"
 				color="--light"
 				max-width="850px"
 				align-self="center"
-				background="#d45353"
+				background="--color-darkL2"
 				border-color="#2163a4"
 			>
-				Hi! I'm a{" "}
-				<Em>
-					heading.
-				</Em>
-				{" "}Click here to add your own text and edit me. It’s a piece of cake.
+				Hey🖐, I'm Ishika Mitra.{"\n\n"}
+			</Text>
+			<Text color="#ffffff" margin="16px 0px 16px 78px" font="32px sans-serif">
+				<br />
+				<br />
+				- A Digital{" "}
+				<Strong background="#da4eff">
+					Product{" "}
+					<Span
+						data-q-widget-type="PRIMITIVE"
+						overflow-wrap="normal"
+						word-break="normal"
+						white-space="normal"
+						text-indent="0"
+						text-overflow="clip"
+						hyphens="manual"
+					>
+						Designer
+					</Span>
+				</Strong>
+				{" "}based in Kolkata, India{"\n\n\n"}
 			</Text>
 		</Section>
 		<Link
